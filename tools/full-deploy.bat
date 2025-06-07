efa1751422c901a755586251256a77aeea90682a
quickairmenu <quickairmenu@gmail.com>
2025-06-07 17:14:45 +0300

    🚀 ixed build issue & updated index.html

--- tools/full-deploy.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-echo 🚀 Starting Full Deploy ...
-
-cd /d "E:\Air Menu\00_Air Menu\WebSite\04\air-menu-website"
-
-npm run deploy-all
-
-pause
---
+++ tools/start-deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+echo 🚀 Starting Deploy Script ...
+
+:: انتقل إلى مسار المشروع
+cd /d "E:\Air Menu\00_Air Menu\WebSite\04\air-menu-website"
+
+:: شغل أمر النشر
+npm run deploy-all
+
+pause
+
+
